b66437984cfa5c16025bc9c02515364468b29807
Harikrishnan Shaji <h73krishnan@gmail.com>
2022-07-02 21:34:35 +0530

    import export - exe w/o promise

---
+++ Promises/importExportES6 - no ex for promise/mainProm.js
@@ -0,0 +1,10 @@
+// import User from "./user.js";
+import {helloJS} from "./user.js"
+
+// const user = new User("Bob", 11);
+// console.log(user);
+
+// user.primeNum();
+// didn't work
+
+helloJS();
---
+++ Promises/importExportES6 - no ex for promise/user.js
@@ -0,0 +1,18 @@
+// export default class User {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// export function primeName(user){
+//     console.log(`User's name is ${user.age}`);
+// }
+
+// export function primeAge(user){
+//     console.log(`User is ${user.age} years old`);
+// }
+
+export const helloJS = () =>{
+    console.log("Here we get the answer");
+}
